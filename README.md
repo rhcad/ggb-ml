a5f4a7b9c14df6c532c38cc71a34895b1c1816e4
Zhang Yungui <rhcad@hotmail.com>
2018-01-01 12:08:53 +0800

    增加练习1
    从GGB素材的标题中提取英文单词及其频次

---
+++ README.md
@@ -0,0 +1,8 @@
+# ggb-ml
+
+使用Python对GeoGebra数据进行机器学习的练习项目。
+
+## 练习
+
+- [sum_titles_1.py](sum_titles_1.py): 从GGB素材的标题中提取英文单词及其频次。
+  ![](screenshot/sum_titles_1.png)
---
+++ sum_titles_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 从GGB素材的标题中提取英文单词及其频次
+
+import sys
+import re
+import json
+import nltk
+import itertools
+from operator import *
+
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+except NameError:
+    pass
+
+# 加载停用词和所有标题
+stops = [s.strip() for s in open('data/stopwords.txt').readlines()]
+titles = json.load(file('data/titles.json'))
+print('%d titles' % len(titles))
+
+# 从标题拆分出单词，去掉数字、标点符号等非英文字符
+tokens = map(lambda t: re.split(r'\s', re.sub(r'[^a-z]', ' ', t)), titles)
+tokens = list(itertools.chain(*tokens))
+# 去掉太短单词和属于停用词的单词
+tokens = list(itertools.ifilter(lambda t: len(t) > 2 and t not in stops, tokens))
+print('%d tokens' % len(tokens))
+
+# 计算单词的分布频率并保存
+freq = nltk.FreqDist(tokens)
+lines = ['%s\t%d\n' % (k, v) for k, v in sorted(freq.items(), key=itemgetter(1), reverse=True)]
+open('data/_titles.txt', 'w').writelines(lines)
+
+# 绘制前N个单词的分布频率折线图（需要安装matplotlib库）
+freq.plot(100)
