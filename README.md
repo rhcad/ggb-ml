cdda0e7eede6a36240a74c9b7301d98fcf591d0d
Zhang Yungui <rhcad@hotmail.com>
2018-01-01 20:47:38 +0800

    比较清华THULAC与结巴中文分词包

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - [sum_titles_2.py](sum_titles_2.py): 从GGB素材的标题中提取所有几何名词及其搭配。
   使用了标识化处理、相邻文本、词性标注等方法。
 
-- [segment_1.py](segment_1.py): 基于清华THULAC中文词法分析工具包试验几题文本解析效果。
+- [segment_1.py](segment_1.py): 基于中文词法分析工具包试验几题文本解析效果。
 
 ## 参考资料
 
--- segment_1.py
+++ segment_1.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 基于清华THULAC中文词法分析工具包试验几题文本解析效果
+# 基于清华THULAC中文词法分析工具和“结巴”中文分词包试验几题文本解析效果
 
 import sys
 import re
 import thulac
+from jieba import analyse
 
 try:
     reload(sys)
     sys.setdefaultencoding('utf-8')
 except NameError:
     pass
+
+sentence = '在△ABC中，AB=AC，△ABC的外接圆⊙O的弦AD的延长线交BC的延长线于点E．求证：△ABD~△AEB．'
 
 tags = '''n/名词 np/人名 ns/地名 ni/机构名 nz/其它专名
 m/数词 q/量词 mq/数量词 t/时间词 f/方位词 s/处所词
@@ -20,6 +23,10 @@ e/叹词 o/拟声词 g/语素 w/标点 x/其它'''
 tags = {t.split('/')[0]: t.split('/')[1] for t in re.split(r'\s|\n', tags)}
 
 thu1 = thulac.thulac()
-c = thu1.cut(u'如图，已知正方形ABCD的对角线AC、BD相交于点O，E是AC上一点，连结EB，过点A作AM⊥BE，垂足为M，AM交BD于点F．求证：OE=OF；')
-c = [text + '/' + tags.get(tag, tag) for text, tag in c]
+c = thu1.cut(sentence)
+c = [text + '/' + tag + tags.get(tag, '') for text, tag in c]
+print('  '.join(c))
+
+# c = jieba.cut(sentence, cut_all=True)  # True完全模式，False精确模式(本例句解析失败)
+c = analyse.extract_tags(sentence)  # https://my.oschina.net/kakablue/blog/314513
 print('  '.join(c))
