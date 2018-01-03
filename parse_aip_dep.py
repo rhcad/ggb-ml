ffc109bda24b1eba543c418fde33d1bd049908dc
Zhang Yungui <rhcad@hotmail.com>
2018-01-04 07:39:21 +0800

    自动翻译依存关系标识

---
+++ parse_aip_dep.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+
+try:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+except NameError:
+    pass
+
+lines = [(re.sub(r'^\d+\.\s*[^A-Z]+|（.+$|\n', '', t),
+          re.sub(r'^\d+\.\s*|[A-Z].+$|\n', '', t))
+         for t in open('data/aip_dep_raw.txt').readlines()
+         if re.search(ur'^\d+\.\s*[\u4e00-\u9fa5]+[A-Z]+', t.decode('utf-8'))]
+open('data/aip_dep.txt', 'w').write('\n'.join(['%s\t%s' % t for t in lines]))
+print(len(lines))
--- segment_2.py
+++ segment_2.py
@@ -18,7 +18,8 @@ sentence = '在△ABC中，AB=AC，△ABC的外接圆⊙O的弦AD的延长线交
 tags = '''n/名词 nr/人名 ns/地名 nt/机构名 nz/其他专名
 m/数词 q/量词 t/时间词 f/方位词 s/处所词 nw/作品名
 v/动词 vd/动副词 vn/名动词 a/形容词 ad/副形词 an/名形词 d/副词
-r/代词 c/连词 p/介词 u/助词 xc/其他虚词 w/标点'''
+r/代词 c/连词 p/介词 u/助词 xc/其他虚词 w/标点
+ng/标点 dg/副语素 ag/形语素 x/字母'''
 tags = {t.split('/')[0]: t.split('/')[1] for t in re.split(r'\s|\n', tags)}
 
 APP_ID, API_KEY, SECRET_KEY = open('data/_aip_key.txt').read().split('\n')
@@ -36,46 +37,50 @@ print('  '.join(c))
 # 点/n名词  E/xc其他虚词  ．/w标点  求证/vn名动词  ：/w标点  △/w标点  ABD/nz其他专名  ~/w标点
 # △/w标点  AEB/nz其他专名  ．/w标点
 
+
 # 依存句法分析
-res = client.depParser(sentence)
-for t in res.get('items', []):
-    print(json.dumps(t, ensure_ascii=False))
+dep = {r.split('\t')[0]: r.split('\t')[1] for r in open('data/aip_dep.txt').read().split('\n')}
+res = client.depParser(sentence)  # 返回值没有id？
+for i, t in enumerate(res.get('items', [])):
+    print('id: %-2d\thead: %-2d\tword: %-6s\tpostag: %-2s %-8s\tdeprel: %-3s %s' %
+          (t.get('id', i), t['head'], t['word'], t['postag'], tags.get(t['postag'], ''),
+           t['deprel'], dep.get(t['deprel'], '')))
 
 '''
-{"postag": "p",  "head": 21, "word": "在", "deprel": "ADV"}
-{"postag": "w",  "head": 3,  "word": "△", "deprel": "ATT"}
-{"postag": "n",  "head": 4,  "word": "ABC", "deprel": "ATT"}
-{"postag": "f",  "head": 1,  "word": "中", "deprel": "POB"}
-{"postag": "w",  "head": 1,  "word": "，", "deprel": "WP"}
-{"postag": "x",  "head": 8,  "word": "AB", "deprel": "ATT"}
-{"postag": "w",  "head": 8,  "word": "=", "deprel": "ATT"}
-{"postag": "n",  "head": 21, "word": "AC", "deprel": "SBV"}
-{"postag": "w",  "head": 8,  "word": "，", "deprel": "WP"}
-{"postag": "w",  "head": 12, "word": "△", "deprel": "DE"}
-{"postag": "n",  "head": 10, "word": "ABC", "deprel": "COO"}
-{"postag": "u",  "head": 13, "word": "的", "deprel": "DE"}
-{"postag": "n",  "head": 8,  "word": "外接圆", "deprel": "COO"}
-{"postag": "ng", "head": 13, "word": "⊙", "deprel": "COO"}
-{"postag": "x",  "head": 16, "word": "O", "deprel": "DE"}
-{"postag": "u",  "head": 17, "word": "的", "deprel": "DE"}
-{"postag": "n",  "head": 19, "word": "弦", "deprel": "DE"}
-{"postag": "n",  "head": 17, "word": "AD", "deprel": "COO"}
-{"postag": "u",  "head": 20, "word": "的", "deprel": "DE"}
-{"postag": "n",  "head": 8,  "word": "延长线", "deprel": "COO"}
-{"postag": "v",  "head": 0,  "word": "交", "deprel": "HED"}
-{"postag": "n",  "head": 23, "word": "BC", "deprel": "DE"}
-{"postag": "u",  "head": 24, "word": "的", "deprel": "DE"}
-{"postag": "n",  "head": 21, "word": "延长线", "deprel": "VOB"}
-{"postag": "p",  "head": 21, "word": "于", "deprel": "CMP"}
-{"postag": "ng", "head": 25, "word": "点", "deprel": "POB"}
-{"postag": "x",  "head": 26, "word": "E", "deprel": "VOB"}
-{"postag": "ng", "head": 26, "word": "．", "deprel": "WP"}
-{"postag": "v",  "head": 26, "word": "求证", "deprel": "COO"}
-{"postag": "w",  "head": 21, "word": "：", "deprel": "WP"}
-{"postag": "w",  "head": 21, "word": "△", "deprel": "IC"}
-{"postag": "n",  "head": 31, "word": "ABD", "deprel": "COO"}
-{"postag": "w",  "head": 31, "word": "~", "deprel": "WP"}
-{"postag": "w",  "head": 31, "word": "△", "deprel": "COO"}
-{"postag": "n",  "head": 34, "word": "AEB", "deprel": "COO"}
-{"postag": "ng", "head": 31, "word": "．", "deprel": "WP"}
+id: 0 	head: 21	word: 在     	postag: p  介词      	deprel: ADV 状中结构
+id: 1 	head: 3 	word: △     	postag: w  标点      	deprel: ATT 定中关系
+id: 2 	head: 4 	word: ABC   	postag: n  名词      	deprel: ATT 定中关系
+id: 3 	head: 1 	word: 中     	postag: f  方位词     	deprel: POB 介宾关系
+id: 4 	head: 1 	word: ，     	postag: w  标点      	deprel: WP  标点
+id: 5 	head: 8 	word: AB    	postag: x  字母      	deprel: ATT 定中关系
+id: 6 	head: 8 	word: =     	postag: w  标点      	deprel: ATT 定中关系
+id: 7 	head: 21	word: AC    	postag: n  名词      	deprel: SBV 主谓关系
+id: 8 	head: 8 	word: ，     	postag: w  标点      	deprel: WP  标点
+id: 9 	head: 12	word: △     	postag: w  标点      	deprel: DE  的字结构
+id: 10	head: 10	word: ABC   	postag: n  名词      	deprel: COO 并列关系
+id: 11	head: 13	word: 的     	postag: u  助词      	deprel: DE  的字结构
+id: 12	head: 8 	word: 外接圆   	postag: n  名词      	deprel: COO 并列关系
+id: 13	head: 13	word: ⊙     	postag: ng 标点      	deprel: COO 并列关系
+id: 14	head: 16	word: O     	postag: x  字母      	deprel: DE  的字结构
+id: 15	head: 17	word: 的     	postag: u  助词      	deprel: DE  的字结构
+id: 16	head: 19	word: 弦     	postag: n  名词      	deprel: DE  的字结构
+id: 17	head: 17	word: AD    	postag: n  名词      	deprel: COO 并列关系
+id: 18	head: 20	word: 的     	postag: u  助词      	deprel: DE  的字结构
+id: 19	head: 8 	word: 延长线   	postag: n  名词      	deprel: COO 并列关系
+id: 20	head: 0 	word: 交     	postag: v  动词      	deprel: HED 核心
+id: 21	head: 23	word: BC    	postag: n  名词      	deprel: DE  的字结构
+id: 22	head: 24	word: 的     	postag: u  助词      	deprel: DE  的字结构
+id: 23	head: 21	word: 延长线   	postag: n  名词      	deprel: VOB 动宾关系
+id: 24	head: 21	word: 于     	postag: p  介词      	deprel: CMP 动补结构
+id: 25	head: 25	word: 点     	postag: ng 标点      	deprel: POB 介宾关系
+id: 26	head: 26	word: E     	postag: x  字母      	deprel: VOB 动宾关系
+id: 27	head: 26	word: ．     	postag: ng 标点      	deprel: WP  标点
+id: 28	head: 26	word: 求证    	postag: v  动词      	deprel: COO 并列关系
+id: 29	head: 21	word: ：     	postag: w  标点      	deprel: WP  标点
+id: 30	head: 21	word: △     	postag: w  标点      	deprel: IC  独立分句
+id: 31	head: 31	word: ABD   	postag: n  名词      	deprel: COO 并列关系
+id: 32	head: 31	word: ~     	postag: w  标点      	deprel: WP  标点
+id: 33	head: 31	word: △     	postag: w  标点      	deprel: COO 并列关系
+id: 34	head: 34	word: AEB   	postag: n  名词      	deprel: COO 并列关系
+id: 35	head: 31	word: ．     	postag: ng 标点      	deprel: WP  标点
 '''
